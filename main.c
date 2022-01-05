9e0b8daada144431a94dd30574a504da6e4b1bea
Sergio Prado <sergio.prado@e-labworks.com>
2022-01-05 18:51:02 -0300

    Add initial implementation
    Signed-off-by: Sergio Prado <sergio.prado@e-labworks.com>

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    printf("What is the answer to the ultimate question of "
+	   "life, the universe, and everything?\n");
+    return 0;
+}
