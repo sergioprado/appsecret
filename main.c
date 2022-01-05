b5fbcf517049f7f087c2331b763607bbdb4a1f30
Sergio Prado <sergio.prado@e-labworks.com>
2022-01-05 19:15:24 -0300

    Use libsecret to get the meaning of life
    Signed-off-by: Sergio Prado <sergio.prado@e-labworks.com>

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <libsecret.h>
 
 int main(int argc, const char *argv[])
 {
     printf("What is the answer to the ultimate question of "
 	   "life, the universe, and everything?\n");
+    printf("The answer is: %d\n", get_meaning_of_life());
     return 0;
 }
